37833621f28f772ce28302202a2ebb4a245d32ee
James Richardson <me+github@james.richardson.name>
2013-07-13 08:59:16 +0100

    major refactoring

--- invoker/CommandInvoker.java
+++ invoker/CommandInvoker.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  Copyright (c) 2013 James Richardson.
 
- Invoker.java is part of bukkit-utilities.
+ CommandInvoker.java is part of bukkit-utilities.
 
  BukkitUtilities is free software: you can redistribute it and/or modify it
  under the terms of the GNU General Public License as published by the Free
@@ -16,18 +16,36 @@
  BukkitUtilities. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
-package name.richardson.james.bukkit.utilities.command;
+package name.richardson.james.bukkit.utilities.command.invoker;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 
 import org.bukkit.command.TabExecutor;
 
 import name.richardson.james.bukkit.utilities.command.Command;
+import name.richardson.james.bukkit.utilities.command.context.AbstractCommandContext;
+import name.richardson.james.bukkit.utilities.command.context.CommandContext;
 
-public interface Invoker extends TabExecutor {
+public interface CommandInvoker extends TabExecutor {
 
+	/**
+	 * Add a command to this CommandInvoker allowing it to delgate arguments to it that match the command's name.
+	 * @param command
+	 */
 	public void addCommand(Command command);
 
+	/**
+	 * Add a collection of commands to this CommandInvoker.
+	 * @param commands
+	 */
+	public void addCommands(Collection<Command> commands);
+
+	/**
+	 * Get a unmodifiable map of all the commands assigned to this invoker.
+	 * @return the commands
+	 */
 	public Map<String, Command> getCommands();
 
 }
